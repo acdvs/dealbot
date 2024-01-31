17fad58a82283f6e0b677019b1de8729d86484d6
Adam Davies <acdavies001@gmail.com>
2024-01-31 16:53:24 -0600

    Simplify logger export

--- lib/util/logger.ts
+++ lib/util/logger.ts
@@ -1,9 +1,7 @@
 import colors from 'colors';
 
-const log = {
+export default {
   msg: (str: string) => console.log(`[${colors.bold.green('LOG')}] ${str}`),
   warn: (str: string) => console.log(`[${colors.bold.yellow('WARN')}] ${str}`),
   error: (str: string) => console.log(`[${colors.bold.red('ERR')}] ${str}`),
 };
-
-export default log;
