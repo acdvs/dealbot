69893d130ce750ac181133c368b7719a1cb79880
Adam <adam@acdvs.dev>
2025-11-09 02:37:27 -0600

    fix(bot): always respond to autocomplete (#49)
    * fix(bot): always respond to autocomplete
    
    * fix(bot): add NODE_ENV to turbo envs

--- apps/bot/src/commands/shared/listings.ts
+++ apps/bot/src/commands/shared/listings.ts
@@ -1,4 +1,5 @@
 import {
+  ApplicationCommandOptionChoiceData,
   AutocompleteInteraction,
   ChatInputCommandInteraction,
   MessageFlags,
@@ -6,11 +7,13 @@ import {
 } from 'discord.js';
 
 import { Bot } from '../../bot';
+import { Command } from '../../command';
 import { ChoicesEmbed } from '../../embeds/choices-embed';
 import { DealsEmbed } from '../../embeds/deals-embed';
 import { Embed } from '../../lib/embed';
-import { countries } from '@dealbot/db/values';
-import { Command } from '../../command';
+import { DEFAULT_COUNTRY_CODE, countries } from '@dealbot/db/values';
+
+const AC_ID_PREFIX = 'AUTOCOMPLETE';
 
 function getListingsCommand({
   name,
@@ -43,31 +46,28 @@ function getListingsCommand({
     async run(ix: ChatInputCommandInteraction) {
       await ix.deferReply();
 
-      const game = ix.options.getString('game', true);
-      const countryCode = ix.options.getString('country') || undefined;
-      const validCountryCode =
-        countryCode &&
-        (countries.find((x) => x.code === countryCode)?.code || 'US');
+      let results: Awaited<ReturnType<typeof Bot.api.search>> = [];
+      let exactResult: string | undefined;
 
-      const results = await Bot.api.search(game, 10);
-      const exactResult = results?.find((x) => x.title === game);
+      const gameInput = ix.options.getString('game', true);
+
+      if (gameInput.startsWith(AC_ID_PREFIX)) {
+        exactResult = gameInput.replace(`${AC_ID_PREFIX}-`, '');
+      } else {
+        results = await Bot.api.search(gameInput, 10);
+        exactResult = results?.find((x) => x.title === gameInput)?.id;
+      }
+
+      const countryCodeInput = ix.options.getString('country');
+      const country = countries.find((x) => x.code === countryCodeInput);
+      const countryCode = country?.code || DEFAULT_COUNTRY_CODE;
 
       if (exactResult) {
-        const embed = new DealsEmbed(
-          ix,
-          exactResult.id,
-          validCountryCode,
-          dealsOnly
-        );
+        const embed = new DealsEmbed(ix, exactResult, countryCode, dealsOnly);
         const messageOptions = await embed.asyncOptions();
         ix.editReply(messageOptions);
       } else if (results && results.length > 0) {
-        const embed = new ChoicesEmbed(
-          ix,
-          results,
-          validCountryCode,
-          dealsOnly
-        );
+        const embed = new ChoicesEmbed(ix, results, countryCode, dealsOnly);
         ix.editReply({
           ...embed.options(),
           flags: [MessageFlags.IsComponentsV2],
@@ -75,7 +75,7 @@ function getListingsCommand({
       } else {
         ix.editReply(
           Embed.basic(
-            `No results were found for "${game}". Did you spell it correctly?`
+            `No results were found for "${gameInput}". Did you spell it correctly?`
           )
         );
       }
@@ -83,26 +83,27 @@ function getListingsCommand({
 
     async autocomplete(ix: AutocompleteInteraction) {
       const focusedOption = ix.options.getFocused(true);
+      const value = focusedOption.value;
 
-      if (focusedOption.name === 'game' && focusedOption.value.length > 1) {
-        const results = await Bot.api.search(focusedOption.value, 25);
-        const suggestions =
-          results?.map((x) => ({ name: x.title, value: x.title })) || [];
+      let suggestions: ApplicationCommandOptionChoiceData[] = [];
 
-        await ix.respond(suggestions);
-        return;
+      if (focusedOption.name === 'game' && value.length > 1) {
+        const results = await Bot.api.search(value, 25);
+        suggestions = results.map((x) => ({
+          name:
+            x.title.length > 100 ? x.title.substring(0, 95) + '...' : x.title,
+          value: `${AC_ID_PREFIX}-${x.id}`,
+        }));
       }
 
       if (focusedOption.name === 'country') {
         const options = countries.map((x) => ({ name: x.name, value: x.code }));
-        const suggestions = options
-          .filter((x) =>
-            x.name.toLowerCase().includes(focusedOption.value.toLowerCase())
-          )
+        suggestions = options
+          .filter((x) => x.name.toLowerCase().includes(value.toLowerCase()))
           .slice(0, 25);
-
-        await ix.respond(suggestions);
       }
+
+      await ix.respond(suggestions);
     },
   });
 }
