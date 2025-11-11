9a405775d4b4762b8d0f93a5ef4847894b69c9c7
Adam <adam@acdvs.dev>
2025-11-11 01:51:57 -0600

    fix(web): incorrect guild links (#48)
    * chore: remove tsbuildinfo from turbo check-types output
    
    * chore: update tsconfig
    
    * chore: add NODE_ENV to turbo globals
    
    * refactor: improve guild selector structure
    
    * fix: handle missing guild icon
    
    * fix: properly detect guild absence on guild page
    
    * fix: use correct link for absent guilds
    
    * fix: use vercel env vars for production
    
    * chore: remove turbo start env vars
    
    * fix: abstract BASE_URL for invite link

--- apps/web/src/app/dashboard/_components/guild-selector.tsx
+++ apps/web/src/app/dashboard/_components/guild-selector.tsx
@@ -4,9 +4,10 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useQuery } from '@tanstack/react-query';
 
+import Grid from './grid';
 import { getGuilds } from '@/actions/discord/user-api';
-import ErrorState from '@/components/state/error';
 import Skeleton from '@/components/ui/skeleton';
+import ErrorState from '@/components/state/error';
 import { cx } from '@/lib/utils';
 
 function GuildSelector() {
@@ -15,58 +16,57 @@ function GuildSelector() {
     queryFn: getGuilds,
   });
 
-  let content;
-
   if (isPending) {
-    content = Array(6)
-      .fill(0)
-      .map((v, i) => (
-        <Guild key={i}>
-          <Skeleton className="size-[45px] rounded-2xl" />
-          <Skeleton className="w-24 h-3 rounded-full" />
-        </Guild>
-      ));
-  } else if (isError) {
+    return (
+      <Grid>
+        {Array(6)
+          .fill(0)
+          .map((_, i) => (
+            <Guild key={i}>
+              <Skeleton className="size-[45px] rounded-2xl" />
+              <Skeleton className="w-24 h-3 rounded-full" />
+            </Guild>
+          ))}
+      </Grid>
+    );
+  }
+
+  if (isError) {
     return (
       <ErrorState
         label="Unable to get servers. Please try again later."
         className="h-40"
       />
     );
-  } else {
-    content = data.map((guild) => (
-      <Link key={guild.id} href={`/dashboard/${guild.id}`}>
-        <Guild className="group justify-between rounded-xl border-1 border-transparent hover:border-foreground/30 active:border-foreground/30 transition-colors">
-          <div className="flex items-center gap-3 overflow-hidden">
-            <Image
-              src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp`}
-              height={45}
-              width={45}
-              alt="guild icon"
-              className="size-[45px] rounded-xl"
-            />
-            <p className="text-center truncate" title={guild.name}>
-              {guild.name}
-            </p>
-          </div>
-          {guild.joined && (
-            <Image
-              src="/avatar.png"
-              height={20}
-              width={20}
-              alt="joined guild"
-              className="grayscale group-hover:grayscale-0 group-active:grayscale-0 transition-all"
-            />
-          )}
-        </Guild>
-      </Link>
-    ));
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-x-4 gap-y-2">
-      {content}
-    </div>
+    <Grid>
+      {data.map((guild) => (
+        <Link
+          key={guild.id}
+          href={guild.joined ? `/dashboard/${guild.id}` : '/invite'}
+        >
+          <Guild className="group justify-between rounded-xl border-1 border-transparent hover:border-foreground/30 active:border-foreground/30 transition-colors">
+            <div className="flex items-center gap-3 overflow-hidden">
+              <GuildIcon {...guild} />
+              <p className="text-center truncate" title={guild.name}>
+                {guild.name}
+              </p>
+            </div>
+            {guild.joined && (
+              <Image
+                src="/avatar.png"
+                height={20}
+                width={20}
+                alt="joined guild"
+                className="grayscale group-hover:grayscale-0 group-active:grayscale-0 transition-all"
+              />
+            )}
+          </Guild>
+        </Link>
+      ))}
+    </Grid>
   );
 }
 
@@ -81,6 +81,34 @@ function Guild({
     <div className={cx('p-2 flex items-center gap-4', className)}>
       {children}
     </div>
+  );
+}
+
+function GuildIcon({
+  icon,
+  id,
+  name,
+}: {
+  icon: string | null;
+  id: string;
+  name: string;
+}) {
+  if (!icon) {
+    return (
+      <div className="size-[45px] flex justify-center items-center text-2xl">
+        {name[0].toUpperCase()}
+      </div>
+    );
+  }
+
+  return (
+    <Image
+      src={`https://cdn.discordapp.com/icons/${id}/${icon}.webp`}
+      height={45}
+      width={45}
+      alt="guild icon"
+      className="size-[45px] rounded-xl"
+    />
   );
 }
 
