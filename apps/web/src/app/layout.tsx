6e533972e188f8cc1cc69e755b3a3bfd3a914e65
Adam Davies <acdavies001@gmail.com>
2025-11-11 01:18:09 -0600

    fix: use vercel env vars for production

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,9 +4,15 @@ import { Rubik } from 'next/font/google';
 import './globals.css';
 import Providers from '@/components/providers';
 
-const METADATA_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
-  ? new URL(process.env.NEXT_PUBLIC_BASE_URL)
-  : undefined;
+const isProduction = process.env.NODE_ENV === 'production';
+const isVercel = process.env.VERCEL === '1';
+
+const VERCEL_BASE_URL = isProduction
+  ? 'https://' + process.env.VERCEL_PROJECT_PRODUCTION_URL
+  : 'https://' + process.env.VERCEL_URL;
+const METADATA_BASE_URL = isVercel
+  ? VERCEL_BASE_URL
+  : process.env.NEXT_PUBLIC_BASE_URL;
 
 const rubik = Rubik({
   variable: '--font-rubik',
