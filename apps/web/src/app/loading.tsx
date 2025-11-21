3a9229e4abb0acefd3c61b4ab6a50750d2c26dce
Adam Davies <acdavies001@gmail.com>
2025-11-20 22:30:19 -0600

    feat: add background to loading fallback

--- apps/web/src/app/loading.tsx
+++ apps/web/src/app/loading.tsx
@@ -2,6 +2,6 @@ import LoadingState from '@/components/state/loading';
 
 export default function LoadingFallback() {
   return (
-    <LoadingState className="fixed w-screen h-screen inset-0 stroke-primary" />
+    <LoadingState className="fixed w-screen h-screen inset-0 bg-background" />
   );
 }
