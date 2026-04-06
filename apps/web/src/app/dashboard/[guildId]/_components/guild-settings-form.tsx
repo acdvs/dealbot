e5727c85bab6a04031ea6bd44f8c1b49f2a5f2cc
Adam Davies <acdavies001@gmail.com>
2026-04-05 23:11:02 -0500

    refactor(web): rework ignored sellers setting

--- apps/web/src/app/dashboard/[guildId]/_components/guild-settings-form.tsx
+++ apps/web/src/app/dashboard/[guildId]/_components/guild-settings-form.tsx
@@ -11,16 +11,15 @@ import LoadingState from '@/components/state/loading';
 import Button from '@/components/ui/button';
 import Select from '@/components/ui/select';
 import useFormState, { type Schema, schema } from '../_hooks/use-form-state';
-import IgnoredSellers from './ignored-sellers';
+import { IgnoredSellers } from './ignored-sellers';
 
 const countryOptions = countries
   .sort((a, b) => (a.name < b.name ? -1 : 1))
   .map((x) => ({ label: x.name, value: x.code }));
 
 function GuildSettingsForm({ guildId }: { guildId: string }) {
-  const { sellers, defaultValues, formState, methods, successVisible } =
-    useFormState(guildId);
-  const { control, handleSubmit, register, reset } = methods;
+  const { formState, form, successVisible } = useFormState(guildId);
+  const { control, handleSubmit, register, reset } = form;
   const { isDirty, isLoading, isSubmitting } = formState;
 
   const submit = async (data: Schema) => {
@@ -32,19 +31,21 @@ function GuildSettingsForm({ guildId }: { guildId: string }) {
     }
   };
 
-  if (isLoading || !sellers || !defaultValues) {
+  if (isLoading) {
     return <LoadingState label="Loading server settings" className="h-40" />;
   }
 
   return (
-    <FormProvider {...methods}>
+    <FormProvider {...form}>
       <Form onSubmit={handleSubmit(submit)} className="flex flex-col gap-6">
         <input {...register('guildId')} required hidden readOnly />
-        <Field.Root>
-          <Field.Label className="leading-none">Country</Field.Label>
-          <Field.Description className="text-foreground-accent leading-none mb-2">
-            Filters results by location
-          </Field.Description>
+        <Field.Root className="flex justify-between">
+          <div>
+            <Field.Label className="leading-none">Country</Field.Label>
+            <Field.Description className="text-foreground-accent leading-none mb-2">
+              Filters results by location
+            </Field.Description>
+          </div>
           <Controller
             name="countryCode"
             control={control}
@@ -58,13 +59,7 @@ function GuildSettingsForm({ guildId }: { guildId: string }) {
             )}
           />
         </Field.Root>
-        <Field.Root>
-          <Field.Label className="leading-none">Ignored sellers</Field.Label>
-          <Field.Description className="text-foreground-accent leading-none mb-2">
-            Hides sellers from all results
-          </Field.Description>
-          <IgnoredSellers items={sellers} />
-        </Field.Root>
+        <IgnoredSellers />
         <div className="flex items-center gap-5">
           <Button
             type="submit"
--- apps/web/src/app/dashboard/[guildId]/_components/ignored-sellers.tsx
+++ apps/web/src/app/dashboard/[guildId]/_components/ignored-sellers.tsx
@@ -1,65 +1,100 @@
 'use client';
 
-import { useEffect } from 'react';
-import { Controller, useFieldArray, useFormContext } from 'react-hook-form';
-
+import { Field } from '@base-ui/react/field';
+import {
+  Controller,
+  type FieldArrayWithId,
+  type FieldValues,
+  type UseFieldArrayAppend,
+  type UseFieldArrayRemove,
+  useFieldArray,
+  useFormContext,
+} from 'react-hook-form';
 import Select from '@/components/ui/select';
+import useSellers from '../_hooks/use-sellers';
 
-export default function IgnoredSellers({ items }: { items: string[] }) {
-  const { control, getValues } = useFormContext();
+function IgnoredSellers() {
   const { fields, append, remove } = useFieldArray({
-    control,
     name: 'ignoredSellers',
   });
 
+  return (
+    <Field.Root>
+      <div className="flex justify-between">
+        <div className="mb-2">
+          <Field.Label className="leading-none">Ignored sellers</Field.Label>
+          <Field.Description className="text-foreground-accent leading-none mb-2">
+            Hides sellers from all results
+          </Field.Description>
+        </div>
+        <IgnoredSellersSelect append={append} />
+      </div>
+      <IgnoredSellersSelected fields={fields} remove={remove} />
+    </Field.Root>
+  );
+}
+
+function IgnoredSellersSelect({
+  append,
+}: {
+  append: UseFieldArrayAppend<FieldValues>;
+}) {
+  const { getValues } = useFormContext();
+  const sellers = useSellers();
+
   const currentValues = getValues('ignoredSellers');
 
-  useEffect(() => {
-    if (currentValues.length === 0) {
-      append(currentValues);
-    }
-  }, [append, currentValues]);
-
-  const selectChoices = items
+  const unselectedOptions = sellers
     .filter((x) => !currentValues.includes(x))
     .map((x) => ({ label: x, value: x }));
 
   return (
-    <div>
-      <Select
-        items={[
-          { label: 'Select ignored seller', value: null },
-          ...selectChoices,
-        ]}
-        value={null}
-        onValueChange={(x) => append(x)}
-        className="mb-2 text-zinc-500 hover:text-foreground transition-colors"
-      />
-      <div className="flex gap-2 flex-wrap">
-        {fields.map((field, idx) => (
-          <div
-            key={field.id}
-            className="group flex items-center gap-2 bg-background-accent pl-4 pr-2 hover:bg-zinc-700 rounded-full cursor-default"
+    <Select
+      items={[
+        { label: 'Select ignored seller', value: null },
+        ...unselectedOptions,
+      ]}
+      value={null}
+      onValueChange={(x) => append(x)}
+      className="mb-2 text-zinc-500 hover:text-foreground transition-colors"
+    />
+  );
+}
+
+function IgnoredSellersSelected({
+  fields,
+  remove,
+}: {
+  fields: FieldArrayWithId[];
+  remove: UseFieldArrayRemove;
+}) {
+  return (
+    <div className="flex gap-2 flex-wrap justify-end">
+      {fields.map((field, idx) => (
+        <div
+          key={field.id}
+          className="group flex items-center gap-2 bg-background-accent pl-4 pr-2 hover:bg-zinc-700 rounded-full cursor-default"
+        >
+          <Controller
+            name={`ignoredSellers.${idx}`}
+            render={({ field }) => (
+              <div>
+                <span className="pt-0.5 text-sm">{field.value}</span>
+                <input {...field} readOnly hidden />
+              </div>
+            )}
+          />
+          <button
+            type="button"
+            onClick={() => remove(idx)}
+            className="p-1 text-zinc-500 group-hover:text-foreground cursor-pointer"
           >
-            <Controller
-              name={`ignoredSellers.${idx}`}
-              render={({ field }) => (
-                <div>
-                  <span className="pt-[2px] text-sm">{field.value}</span>
-                  <input {...field} readOnly hidden />
-                </div>
-              )}
-            />
-            <button
-              type="button"
-              onClick={() => remove(idx)}
-              className="p-1 text-zinc-500 group-hover:text-foreground cursor-pointer"
-            >
-              x
-            </button>
-          </div>
-        ))}
-      </div>
+            x
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
+
+export { IgnoredSellers };
