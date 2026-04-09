92f7f694d896aeac3bbd8b47bce68d8ef429f361
Adam Davies <adam@acdvs.dev>
2026-04-09 01:51:15 -0500

    feat(web): prefetch guild settings

--- apps/web/src/app/dashboard/[guildId]/page.tsx
+++ apps/web/src/app/dashboard/[guildId]/page.tsx
@@ -1,5 +1,8 @@
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { RedirectType, redirect } from 'next/navigation';
 import { getGuilds } from '@/actions/discord/user-api';
+import { getGuildSettings } from '@/actions/guild';
+import { getSellers } from '@/actions/itad-api';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -16,21 +19,33 @@ export default async function GuildPage({
 }: {
   params: Promise<{ guildId: string }>;
 }) {
+  const { guildId } = await params;
   const queryClient = getQueryClient();
+
   const guilds = await queryClient.fetchQuery({
     queryKey: ['guilds'],
     queryFn: getGuilds,
   });
 
-  const { guildId } = await params;
   const guild = guilds?.find((x) => x.id === guildId);
 
   if (!guild || !guild.joined) {
     redirect('/', RedirectType.replace);
   }
 
+  await queryClient.prefetchQuery({
+    queryKey: ['guild-settings', guildId],
+    queryFn: async () => getGuildSettings(guildId),
+  });
+
+  await queryClient.prefetchQuery({
+    queryKey: ['sellers'],
+    queryFn: getSellers,
+    staleTime: 1000 * 60 * 60 * 24,
+  });
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <title>{`Dealbot | ${guild.name}`}</title>
       <Breadcrumb className="mb-2">
         <BreadcrumbList>
@@ -45,6 +60,6 @@ export default async function GuildPage({
       </Breadcrumb>
       <h1 className="mb-5 sm:mb-10">{guild.name}</h1>
       <SettingsForm guildId={guildId} />
-    </>
+    </HydrationBoundary>
   );
 }
